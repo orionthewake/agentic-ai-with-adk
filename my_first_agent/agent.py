dd843bed4b236870035650dc4e6f25b00da99818
Joseph Howard <joe.howard@mac.com>
2026-03-05 08:00:45 -0500

    feat(my_first_agent): integrate Google Search tool

--- my_first_agent/agent.py
+++ my_first_agent/agent.py
@@ -1,10 +1,12 @@
 from google.adk.agents import Agent
+from google.adk.tools import google_search
 
 root_agent = Agent(
     name="my_first_agent",
     model="gemini-3-flash-preview",
-    description="An example agent that will answer a user query related to Google Cloud",
+    description="An example agent that will answer a user query based on Google Search",
     instruction="""
-    First ask for the user's name & start a conversation by greeting the user with name.You are an AI assistant that helps users with Google Cloud related queries.
+    First ask for the user's name & start a conversation by greeting the user with name.You are an AI assistant that helps users with Google Cloud related queries based on Google Search.
     """,
+    tools=[google_search],
 )
